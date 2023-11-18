ce540589e1c1d1d3dded02eb0c152e6223e2be58
Valentina Klochkova <KlochkovaValentina11@yandex.ru>
2023-11-18 22:15:16 +0300

    commit 18/11/23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,44 +13,46 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        List<Employee> list = parseCSV(columnMapping, "Users/Oleg/IdeaProjects/CSV_Parser/src/main/resources/data.csv");
+ //       List<Employee> list = parseCSV(columnMapping, "Users/Oleg/IdeaProjects/CSV_Parser/src/main/resources/data.csv");
 
         System.out.println("=== write() ===");
-        write("data.csv");
+     //   write("data.csv");
     //    read();
         readToObjects();
         String json = listToJson(list);
 
-    }
-    private static void write(String fileName) {
-        var employee = "1,John,Smith,USA,25"; "2,Inav,Petrov,RU,23".split(",");
-        try (CSVWriter csvWriter = new CSVWriter(
-                new FileWriter(fileName))
-        ) {
-            csvWriter.writeNext(new String[]{employee});
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
-    private static void parseCSV(String columnMapping, String fileName ) {
-        try (CSVReader csvReader = new CSVReader(
-                new FileReader(fileName))
-        ) {
-            String[] nextLine;
-            while ((nextLine = csvReader.readNext()) != null) {
-                System.out.println(Arrays.toString(nextLine));
-            }
-        } catch (CsvValidationException | IOException e) {
-            System.out.println(e.getMessage());
-        }
     }
+ //   private static void write(String fileName) {
+  //      var employee = "1,John,Smith,USA,25"; "2,Inav,Petrov,RU,23".split(",");
+  //      try (CSVWriter csvWriter = new CSVWriter(
+  //              new FileWriter(fileName))
+  //      ) {
+   //         csvWriter.writeNext(new String[]{employee});
+  //      } catch (IOException e) {
+  //          System.out.println(e.getMessage());
+  //      }
+ //   }
+
+  //  private static void parseCSV(String columnMapping, String fileName ) {
+  //      try (CSVReader csvReader = new CSVReader(
+  //              new FileReader(fileName))
+   //     ) {
+  //          String[] nextLine;
+  //          while ((nextLine = csvReader.readNext()) != null) {
+  //              System.out.println(Arrays.toString(nextLine));
+   //         }
+   //     } catch (CsvValidationException | IOException e) {
+   //         System.out.println(e.getMessage());
+   //     }
+  //  }
 
     private static List<Employee> readToObjects() {
         try (CSVReader csvReader = new CSVReader(new FileReader("Users/Oleg/IdeaProjects/CSV_Parser/src/main/resources/data.csv"))) {
@@ -74,13 +76,14 @@ public class Main {
 
 GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        Type listType = new TypeToken<List<T>>() {}.getType();
-        String json = gson.toJson(list);
+        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        String json = gson.toJson(listType);
+        System.out.println(json + list);
 
-        try (FileWriter fileWriter = new FileWriter("Users/Oleg/IdeaProjects/CSV_Parser/src/main/resources/data.json")) {
-            fileWriter.write(json);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+ //       try (FileWriter fileWriter = new FileWriter("Users/Oleg/IdeaProjects/CSV_Parser/src/main/resources/data.json")) {
+ //           fileWriter.write(json);
+  //      } catch (IOException e) {
+  //          System.out.println(e.getMessage());
+  //      }
+  //  }
 }
